a3f4f6d8c47ecb257430b9d1a36b69a0ec770bed
RadekVlcek <radoslav.vlcek@gmail.com>
2018-12-20 00:29:56 +0100

    moving on...

--- js/game.js
+++ js/game.js
@@ -1,25 +1,29 @@
-/**
- * Possible squares:
- * 16
- * 36
- * 64
- */
-
-HTMLtarget = document.getElementById('target');
+window.onload = init;
 
 var cardsAmount = 16,
     cards1 = [],
-    cards2 = [];
+    cards2 = [],
+    sCards,
+    isWaiting = false,
+    lock = false,
+    firstCard = { src: '', id: '' },
+    secondCard = { src: '', id: '' },
+    intervalMatch,
+    intervalNotMatch,
+    output = '',
+    inc = 0,
+    sqrt = Math.sqrt(cardsAmount),
+    defCard = 'secret.png',
+    HTMLstart = document.getElementById('start'),
+    HTMLtarget = document.getElementById('target');
 
-for(let x=0 ; x<cardsAmount/2 ; x++){
-    cards1.push({src: x+1});
-    cards2.push({src: x+1});
+function init(){
+    HTMLstart.addEventListener('click', prepCards);
 }
 
 // In order to achvieve efficient permutation we need to use Fisher-Yates shuffle algorithm
-function shuffle(cards) {
+function shuffleCards(cards) {
     var j, x, i;
-
     for (i=cards.length-1; i>0; i--){
         j = Math.floor(Math.random() * (i + 1));
         x = cards[i];
@@ -30,33 +34,20 @@ function shuffle(cards) {
     return cards;
 }
 
-// Merge both arrays and shuffle elements
-var sCards = shuffle(cards1.concat(cards2));
-
-var output = '',
-    inc = 0,
-    sqrt = Math.sqrt(cardsAmount),
-    defCard = 'secret.png';
-
-for(let row=0 ; row<sqrt ; row++){
-    for(let col=0 ; col<sqrt ; col++){
-        output += `<li class="card" id=${inc} style="background-image:url('/cards/${defCard}');" onclick=decide(id)></li>`;
-        inc++;
+// Print cards
+function printCards(){
+    for(let row=0 ; row<sqrt ; row++){
+        for(let col=0 ; col<sqrt ; col++){
+            output += `<li class="card" id=${inc} style="background-image:url('/cards/${defCard}');" onclick=playCard(id)></li>`;
+            inc++;
+        }
+        output += '<br>';
     }
-    output += '<br>';
+    HTMLtarget.innerHTML = output;
 }
 
-HTMLtarget.innerHTML = output;
-
-var isWaiting = false,
-    lock = false,
-    firstCard = { src: '', id: '' },
-    secondCard = { src: '', id: '' },
-    intervalMatch,
-    intervalNotMatch;
-// console.log(intervalMatch);
-
-function decide(id){
+// Take action when clicked on a card
+function playCard(id){
     if(!lock){
         let getSrc = sCards[id].src;
 
@@ -74,16 +65,16 @@ function decide(id){
         else {
             secondCard.src = getSrc;
             secondCard.id = id;
-            lock = true;
 
             // match
             if(firstCard.src == secondCard.src){
                 if(firstCard.id != secondCard.id){
+                    lock = true;
                     intervalMatch = setTimeout(function(){
                         document.getElementById(firstCard.id).style.visibility = 'hidden';
                         document.getElementById(secondCard.id).style.visibility = 'hidden';
                         lock = false;
-                    }, 800);
+                    }, 700);
                 }
 
                 else return;
@@ -91,19 +82,33 @@ function decide(id){
 
             // not match
             else {
+                lock = true;
                 intervalNotMatch = setTimeout(function(){
                     document.getElementById(firstCard.id).style.backgroundImage = `url(/cards/${defCard})`;
                     document.getElementById(secondCard.id).style.backgroundImage = `url(/cards/${defCard})`;
                     lock = false;
-                }, 800);   
+                }, 700);
             }
 
             isWaiting = false;
         }
     }
 
-    else {
-        console.log('LOCKED.')
-        return;
+    else return;
+}
+
+// Prepare cards
+function prepCards(){
+    HTMLtarget.style.display = 'block';
+    HTMLstart.style.display = 'none';
+
+    for(let x=0 ; x<cardsAmount/2 ; x++){
+        cards1.push({src: x+1});
+        cards2.push({src: x+1});
     }
+
+    // Merge both arrays and shuffle elements
+    sCards = shuffleCards(cards1.concat(cards2));
+
+    printCards();
 }
